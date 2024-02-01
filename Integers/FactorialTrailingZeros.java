810f572a0f68c71c6226f02573ca410f2c229042
Marcin Lukowski <marcin.lukowkski@axbit.com>
2024-02-01 20:48:48 +0100

    .

---
+++ Integers/FactorialTrailingZeros.java
@@ -0,0 +1,14 @@
+package Integers;
+
+/*
+ * Given an integer n, return the number of trailing zeros in n!
+ * 
+ * n! -> 1*2....n-1*n
+ */
+
+public class FactorialTrailingZeros {
+
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
--- Integers/Palindrome.java
+++ Integers/Palindrome.java
@@ -4,13 +4,30 @@ package Integers;
  * Given an integer x retun true if is a palindrome, and otherwise false
  * 
  * - do it without convert to string
+ * 
+ *  12321 - true
+ *  1221  - true
+ * 
+ * -121 - false
  */
 
 public class Palindrome {
 
     public boolean evaluate(int x) {
-        return true;
+        if (x < 0)
+            return false;
+        if (x != 0 && x % 10 == 0)
+            return false;
+
+        int reverse = 0;
+        while (x > reverse) {
+            int lastDigit = x % 10;
+            reverse = reverse * 10 + lastDigit;
+            x = x / 10;
+        }
+        return x == reverse || (x == reverse / 10);
     }
+    // TC: O(log n), SC: O(1)
 
     public static void main(String[] args) {
         Palindrome p = new Palindrome();
